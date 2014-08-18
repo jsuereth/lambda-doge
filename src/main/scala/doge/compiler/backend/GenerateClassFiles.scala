e7cc51b5bec95517cd0c1d3b4b46cc95b204d39d
Josh Suereth <joshua.suereth@gmail.com>
2014-08-18 15:54:20 -0400

    Swap evil state to be scalaz state.

--- src/main/scala/doge/compiler/backend/GenerateClassFiles.scala
+++ src/main/scala/doge/compiler/backend/GenerateClassFiles.scala
@@ -1,5 +1,6 @@
 package doge.compiler.backend
 
+import org.objectweb.asm.Opcodes._
 import org.objectweb.asm._
 import doge.compiler.types._
 import doge.compiler.types.TypeSystem.{
@@ -12,6 +13,229 @@ import scalaz._
 import Scalaz._
 
 
+case class MethodWriterState(className: String, mv: MethodVisitor, stackNameToIndex: Map[String, Int])
+
+object MethodWriter {
+
+  def localVarIndex(name: String) = State[MethodWriterState, Option[Int]] { mws =>
+    // TODO - Possibly error out here?
+    (mws, mws.stackNameToIndex.get(name))
+  }
+
+
+  /** Loads a local variable off the variable stack. */
+  def loadLocalVariable(tpe: Type, idx: Int) = State[MethodWriterState, Unit] { mws =>
+    tpe match {
+      case TypeSystem.Bool => mws.mv.visitVarInsn(ILOAD, idx)
+      case Integer => mws.mv.visitVarInsn(ILOAD, idx)
+      case _ =>
+        // We assume everything else is an object.
+        mws.mv.visitVarInsn(ALOAD, idx)
+    }
+    (mws, ())
+  }
+
+  /**
+   * Loads a local variable off the local variable stack, by name.
+   *
+   * Throws an exception if the name does not exist.
+   * @param tpe
+   * @param name
+   * @return
+   */
+  def loadLocalVariable(tpe: Type, name: String): State[MethodWriterState, Unit] =
+    for {
+      idx <- localVarIndex(name)
+      _ <- loadLocalVariable(tpe, idx.getOrElse(sys.error(s"Could not find local variable $name")))
+    } yield ()
+
+
+  // Injects the bytecode so the value returned by the given AST is pushed onto the stack.
+  def loadConstant(i: AnyRef) = State[MethodWriterState, Unit] { state =>
+    state.mv.visitLdcInsn(i)
+    state -> ()
+  }
+
+  /** using the IFEQ jump instruction, check the top of the stack and either jump or not. */
+  def jumpIfEq(l: Label) = State[MethodWriterState, Unit] { state =>
+    state.mv.visitJumpInsn(IFEQ, l)
+    state -> ()
+  }
+
+  /** Goto the given label. */
+  def goto(l: Label) = State[MethodWriterState, Unit] { state =>
+    state.mv.visitJumpInsn(GOTO, l)
+    state -> ()
+  }
+
+  /** Writes a label at the current bytecode location, for jump instructions. */
+  def writeLabel(l: Label) = State[MethodWriterState, Unit] { state =>
+    state.mv.visitLabel(l)
+    state -> ()
+  }
+
+
+  /** Will invoke a method defined in the current classfile, with the given type. */
+  def callStaticLocalMethod(name: String, tpe: Type) = State[MethodWriterState, Unit] { state =>
+    state.mv.visitMethodInsn(INVOKESTATIC, state.className, name, GenerateClassFiles.getFunctionSignature(tpe))
+    state -> ()
+  }
+
+  def iadd() = State[MethodWriterState, Unit] { state =>
+    state.mv.visitInsn(IADD)
+    state -> ()
+  }
+
+  /** Places the result of an Ast expression onto the JVM stack.
+    *
+    * Note: This will either:
+    *    * load a constant
+    *    * Call a function
+    *    * Lookup a local variable
+    *    * run inlined function code (e.g. if method).
+    * @param ast
+    * @return
+    */
+  def placeOnStack(ast: TypedAst): State[MethodWriterState, Unit] = {
+    ast match {
+      case i: IntLiteralTyped => loadConstant(new java.lang.Integer(i.value))
+      case b: BoolLiteralTyped => loadConstant(new java.lang.Boolean(b.value))
+      // TODO - how to handle id references?
+      case i: IdReferenceTyped =>
+        for {
+          idx <- localVarIndex(i.name)
+          _ <- idx match {
+            case Some(idx) => loadLocalVariable(i.tpe, idx)
+            case _ =>
+              // Here we assume any non-local variable reference is a local
+              // method call.  TODO - Allow non-local method calls/referencing members.
+              callStaticLocalMethod(i.name, i.tpe)
+          }
+        } yield ()
+      case other =>
+        writeStackInstructions(other)
+    }
+  }
+
+  /** calls a function with a given reference and set of arguments. */
+  def applyFunction(i: IdReferenceTyped, args: Seq[TypedAst]): State[MethodWriterState, Unit] = {
+    type S[A] = State[MethodWriterState, A]
+    for {
+      _ <- args.reverse.toList.traverse[S, Unit](placeOnStack)
+      _ <- callStaticLocalMethod(i.name, i.tpe)
+    } yield ()
+
+  }
+
+  /** Writes the stack instructions for a given AST. */
+  def writeStackInstructions(ast: TypedAst): State[MethodWriterState, Unit] = {
+    ast match {
+      // First the simple expressions.
+      case i: LiteralTyped => placeOnStack(i)
+
+      // HERE we check for hardcoded methods.
+      case ApExprTyped(id, Seq(left, right), _) if id.name == "Plus" => builtInFunctions.plus(left, right)
+      case ApExprTyped(i, Seq(id), _) if i.name == "IS" => writeStackInstructions(id)
+      case ApExprTyped(i, Seq(check, left, right), tpe) if i.name == "ifs" => builtInFunctions.ifs(check, left, right, tpe)
+      case ApExprTyped(id, args, _) if id.name == "PrintLn" => builtInFunctions.println(args)
+
+
+      // This method is not built in.  For now, we assume any non-built-in method is defined
+      // on the same classfile.
+      // TODO - Handle non-local methods
+      case ApExprTyped(id, args, _) => applyFunction(id, args)
+    }
+  }
+
+
+  /** TOOD - Unifiy with however Java types are exposed in the typesystem */
+  def getStatic(className: String, field: String, tpeString: String) = State[MethodWriterState, Unit] { state =>
+    state.mv.visitFieldInsn(GETSTATIC, className, field, tpeString)
+    state -> ()
+  }
+
+  // TODO - Unify this with Java types as exposed in the typesystem later.
+  def invokeVirtual(className: String, methodName: String, tpe: Type) = State[MethodWriterState, Unit] { state =>
+    state.mv.visitMethodInsn(INVOKEVIRTUAL, className, methodName, GenerateClassFiles.getFunctionSignature(tpe))
+    state -> ()
+  }
+
+
+  object builtInFunctions {
+    /** Plus: int => int => int */
+    def plus(left: TypedAst, right: TypedAst): State[MethodWriterState, Unit] =
+      for {
+        _ <- placeOnStack(left)
+        _ <- placeOnStack(right)
+        _ <- iadd()
+      } yield ()
+
+    /** Loads the stdout variable. */
+    def stdout = getStatic("java/lang/System", "out", "Ljava/io/PrintStream;")
+    /** Prints an expression. */
+    def print(in: TypedAst): State[MethodWriterState, Unit] =
+     for {
+       out <- stdout
+       _ <- placeOnStack(in)
+       // TODO - convert to string if not already string.
+       _ <- invokeVirtual("java/io/PrintStream", "print", TypeSystem.Function(in.tpe, TypeSystem.Unit))
+     } yield ()
+    /** Writes the println method. */
+    def println(args: Seq[TypedAst]): State[MethodWriterState, Unit] = {
+      type S[A] = State[MethodWriterState, A]
+      for {
+        _ <- args.toList.traverse[S, Unit](print)
+        _ <- emptyPrintln()
+      } yield ()
+    }
+
+    /** Prints an empty line. */
+    def emptyPrintln() =
+      stdout flatMap { _ =>
+        State[MethodWriterState,Unit] { state =>
+          state.mv.visitMethodInsn(INVOKEVIRTUAL, "java/io/PrintStream", "println", "()V")
+          state -> ()
+        }
+      }
+    /** Encoding on the JVM of "ifs" method, a.k.a. IF statement. */
+    def ifs(check: TypedAst, left: TypedAst, right: TypedAst, resultType: Type): State[MethodWriterState, Unit] = {
+      val fLabel = new Label()
+      val fEndLabel = new Label()
+      for {
+        _ <- placeOnStack(check)
+        _ <- jumpIfEq(fLabel)
+        _ <- placeOnStack(left)
+        _ <- goto(fEndLabel)
+        _ <- writeLabel(fLabel)
+        _ <- placeOnStack(right)
+        _ <- writeLabel(fEndLabel)
+      } yield ()
+    }
+  }
+
+  /** writes a method definition using the given state, returning the last expression result. */
+  def writeMethod(defn: TypedAst): State[MethodWriterState, Unit] = {
+    writeStackInstructions(defn).flatMap { _ =>
+      State[MethodWriterState, Unit] { state =>
+        // TODO - This should return the result of the expression type, not just integer always.
+        defn.tpe match {
+          case TypeSystem.Integer => state.mv.visitInsn(IRETURN)
+          case TypeSystem.Bool => state.mv.visitInsn(IRETURN)
+          case TypeSystem.Unit => state.mv.visitInsn(RETURN)
+          // Everything else is an object
+          case _ => state.mv.visitInsn(ARETURN)
+        }
+        // We need this here, even if it's wrong, as ASM will calculate it appropriately.
+        state.mv.visitMaxs(1, 1);
+        state.mv.visitEnd()
+        state -> ()
+      }
+    }
+  }
+
+}
+
+// TODO - Rename to classfileWriter, possibly with its own state.
 object GenerateClassFiles {
   import Opcodes._
 
@@ -53,10 +277,8 @@ object GenerateClassFiles {
       "([Ljava/lang/String;)V",
       null,
       null);
-    writeStackInstructions(defn, new EvilMethodWriterState(mv, className))
-    mv.visitInsn(RETURN);
-    mv.visitMaxs(2, 1);
-    mv.visitEnd();
+    val state = MethodWriterState(className, mv, Map.empty)
+    MethodWriter.writeMethod(defn)(state)
   }
 
   private def writeMethod(ast: LetExprTyped, name: String, cw: ClassWriter): Unit = {
@@ -70,137 +292,29 @@ object GenerateClassFiles {
          signature,
          null,
          null)
-      writeMethodinstructions(ast.argNames, ast.definition, new EvilMethodWriterState(mv, name))
-      mv.visitEnd()
+      val state = MethodWriterState(name, mv, ast.argNames.zipWithIndex.toMap)
+      MethodWriter.writeMethod(ast.definition)(state)
     }
   }
 
-  // TODO - use scalaz state
-  class EvilMethodWriterState(val mv: MethodVisitor, val className: String) {
-    private val lookUps = new java.util.HashMap[String, Int]
-    def setConstant(name: String, idx: Int): Unit = {
-      lookUps.put(name, idx)
-    }
-    def getConstantLocation(name: String): Option[Int] =
-      Option(lookUps.get(name))
+
+  private def writeClassFile(f: File, cw: ClassWriter): Unit = {
+    val out = new FileOutputStream(f)
+    try out.write(cw.toByteArray)
+    finally out.close()
   }
 
-  /** Writes the bytecode instructions for a method defined with the given Ast, and having said argument names. */
-  private def writeMethodinstructions(argNames: Seq[String], defn: TypedAst, state: EvilMethodWriterState): Unit = {
-    // First update evil state with method argument locations.
-    for((arg, idx) <- argNames.zipWithIndex) {
-      state.setConstant(arg, idx)
-    }
-    writeStackInstructions(defn, state)
-    //mv.visitLdcInsn(1)
-    state.mv.visitInsn(IRETURN)
-    // We need this here, even if it's wrong, as ASM will calculate it appropriately.
-    state.mv.visitMaxs(1, 1);
+
+  def isMainMethod(m: LetExprTyped): Boolean =
+    (m.name == "main") && (m.tpe == Unit)
+
+
+  def main(args: Array[String]): Unit = {
+    import TypeSystem._
+    val test = FunctionN(Integer, Integer, Integer)
+    println(s"$test signature = ${getFunctionSignature(test)}")
   }
 
-  //TODO - validate this doesn't suck at some point.
-  /** Encoding on the JVM of "ifs" method, a.k.a. IF statement. */
-  private def writeIfStatement(check: TypedAst, left: TypedAst, right: TypedAst, resultType: Type, state: EvilMethodWriterState): Unit = {
-    val fLabel = new Label()
-    val fEndLabel = new Label()
-    placeOnStack(check, state)
-    state.mv.visitJumpInsn(IFEQ, fLabel)
-    placeOnStack(left,state)
-    state.mv.visitJumpInsn(GOTO, fEndLabel)
-    state.mv.visitLabel(fLabel)
-    placeOnStack(right, state)
-    state.mv.visitLabel(fEndLabel)
-  }
-
-  /** Encoding on the JVM of the Plus method. */
-  def writePlusStatement(left: TypedAst, right: TypedAst, state: EvilMethodWriterState): Unit = {
-    placeOnStack(left, state)
-    placeOnStack(right, state)
-    state.mv.visitInsn(IADD)
-  }
-
-  def writePrintlnStatement(args: Seq[TypedAst], state: EvilMethodWriterState): Unit = {
-    // TODO - append all to a string, then print that.
-    // SUPER LAZY IMPL TIME.
-    import state.mv
-    def getSystemOut() = mv.visitFieldInsn(GETSTATIC,
-      "java/lang/System",
-      "out",
-      "Ljava/io/PrintStream;");
-    // TODO - Coerce args into strings
-    for(arg <- args) {
-      getSystemOut
-      placeOnStack(arg, state)
-      // TODO - look up the correct type, for now we only support Int.
-      mv.visitMethodInsn(INVOKEVIRTUAL,
-        "java/io/PrintStream",
-        "print",
-        "(I)V"
-      )
-    }
-    // Finally we just println
-    getSystemOut
-    mv.visitMethodInsn(INVOKEVIRTUAL,
-      "java/io/PrintStream",
-      "println",
-      "()V");
-  }
-
-  // Dumps the stack instructors to ge the value of a TypedAst pushed on to the stack.
-  private def writeStackInstructions(defn: TypedAst, state: EvilMethodWriterState): Unit =
-    defn match {
-      // Literals are easiest for now...
-      case i: LiteralTyped => placeOnStack(i, state)
-      // Plus is hardcoded
-      case ApExprTyped(id, Seq(left, right), _) if id.name == "Plus" => writePlusStatement(left, right, state)
-      // IS is erased to the underyling expression.
-      case ApExprTyped(i, Seq(id), _) if i.name == "IS" => writeStackInstructions(id, state)
-      // "if" hardcoded.
-      case ApExprTyped(i, Seq(check, left, right), tpe) if i.name == "ifs" => writeIfStatement(check, left, right, tpe, state)
-      // Println is hardcoded
-      case ApExprTyped(id, args, _) if id.name == "PrintLn" => writePrintlnStatement(args, state)
-      // This method is not built in.  For now, we assume any non-built-in method is defined
-      // on the same classfile.
-      case ApExprTyped(id, args, _) =>
-        // Now we should call the method. TODO - we should check our information about the
-        // type of the method to see if it's a static method or other.
-        args.foreach(arg => placeOnStack(arg, state))
-        state.mv.visitMethodInsn(INVOKESTATIC,
-          state.className,
-          id.name,
-          getFunctionSignature(id.tpe))
-    }
-
-  // Injects the bytecode so the value returned by the given AST is pushed onto the stack.
-  private def placeOnStack(ast: TypedAst, state: EvilMethodWriterState): Unit =
-    ast match {
-      case i: IntLiteralTyped => state.mv.visitLdcInsn(i.value)
-      case b: BoolLiteralTyped => state.mv.visitLdcInsn(b.value)
-      // TODO - how to handle id references?
-      case i: IdReferenceTyped =>
-        state.getConstantLocation(i.name) match {
-          case Some(idx) => typeToLoadInstruction(state.mv, i.tpe, idx)
-          case _ => // Call the method?
-            // Here we assume this is a previous let expression.
-            state.mv.visitMethodInsn(INVOKESTATIC,
-              state.className,
-              i.name,
-              getFunctionSignature(i.tpe))
-        }
-      case other => writeStackInstructions(other, state)
-    }
-
-  // Injects the "load" instruction to pull a local at the given index, with the given type back
-  // onto the stack.
-  def typeToLoadInstruction(mv: MethodVisitor, tpe: Type, idx: Int): Unit = tpe match {
-    case TypeSystem.Bool => mv.visitVarInsn(ILOAD, idx)
-    case Integer => mv.visitVarInsn(ILOAD, idx)
-    case _ =>
-      // We assume everything else is an object.
-      mv.visitVarInsn(ALOAD, idx)
-  }
-
-  // TODO - Support functions w/ generic parameters.
   def getFunctionSignature(tpe: Type): String = {
     import TypeSystem.Function
     val signature = new SignatureWriter()
@@ -225,26 +339,10 @@ object GenerateClassFiles {
       // We don't really support passing functions yet.
       case f @ TypeSystem.Function(_, _) => signature.visitBaseType('A')
       case _ => sys.error(s"Unsupported type: $tpe")
+
     }
     visitFunctionSignature(signature, tpe)
     signature.toString
   }
 
-
-  private def writeClassFile(f: File, cw: ClassWriter): Unit = {
-    val out = new FileOutputStream(f)
-    try out.write(cw.toByteArray)
-    finally out.close()
-  }
-
-
-  def isMainMethod(m: LetExprTyped): Boolean =
-    (m.name == "main") && (m.tpe == Unit)
-
-
-  def main(args: Array[String]): Unit = {
-    import TypeSystem._
-    val test = FunctionN(Integer, Integer, Integer)
-    println(s"$test signature = ${getFunctionSignature(test)}")
-  }
 }
